c6f6265f5522d74a8cd5027f641211f86ed635f4
mochaaP <git@mochaa.ws>
2021-03-13 12:52:31 +0800

    feat: remove annoying mpv.net logo

--- .init.js
+++ .init.js
@@ -1,1 +1,5 @@
-mp.module_paths.push("~~/scripts/modules.js")
+mp.module_paths.push("~~/scripts/modules.js");
+
+mp.register_idle(function() {
+  setTimeout(function() {mp.command("overlay-remove 0")}, 100);
+})
